2e301afabd24ebf68d4b9959354d31b87db04a41
Teema Luksameset <tm@odds.team>
2020-01-05 01:41:00 +0700

    start project

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,6 +6,7 @@ import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
 
+
 @NgModule({
   imports: [
     CommonModule,
@@ -16,7 +17,7 @@ import { HomePage } from './home.page';
         path: '',
         component: HomePage
       }
-    ])
+    ]),
   ],
   declarations: [HomePage]
 })
